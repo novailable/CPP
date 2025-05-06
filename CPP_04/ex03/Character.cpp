1045a339422ccbe7504a8466ee65dadf1fa6720f
novailable <97833342+novailable@users.noreply.github.com>
2025-05-06 17:23:53 +0800

    updated on ex03

--- CPP_04/ex03/Character.cpp
+++ CPP_04/ex03/Character.cpp
@@ -10,13 +10,51 @@ Character::Character(const Character &other)
 {
 	_name = other._name;
 	for (int i = 0; i < 4; ++i)
+		_inventory[i] = other._inventory[i] ? other._inventory[i]->clone() : 0;
+}
+
+Character	&Character::operator=(const Character &other)
+{
+	if (this != &other)
 	{
-		if (other._inventory[i])
-			_inventory[i] = other._inventory[i]->clone();
-		else
-			_inventory[i] = 
-		
+		_name = other._name;
+		for (int i = 0; i < 4; ++i)
+		{
+			delete _inventory[i];
+			_inventory[i] = other._inventory[i] ? other._inventory[i]->clone() : 0;
+		}
+	}
+	return (*this);
+}
+
+Character::~Character()
+{
+	for (int i = 0; i < 4; ++i)
+		delete	_inventory[i];
+}
+
+std::string const	&Character::getName() const {return (_name);}
+
+void	Character::equip(AMateria* m)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		if (!_inventory[i])
+		{
+			_inventory[i] = m;
+			break ;
+		}
 	}
 }
 
-Character::Character(const Character &other)
+void	Character::unequip(int idx)
+{
+	if (idx >= 0 && idx < 4)
+		_inventory[idx] = 0;
+}
+
+void	Character::use(int idx, ICharacter& target)
+{
+	if (idx >= 0 && idx < 4 && _inventory[idx])
+		_inventory[idx]->use(target);
+}
