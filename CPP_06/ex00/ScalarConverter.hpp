4c1eed3bfed0d5d8c0d3bbca87a8c769ce77c253
novailable <novailable@icloud.com>
2025-08-22 16:57:55 +0800

    updated CPP_06

---
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef	SCALARCONVERTER_HPP
+#define	SCALARCONVERTER_HPP
+
+class	ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &other);
+		ScalarConverter	&operator=(const ScalarConverter &other);
+		~ScalarConverter();
+
+		static	void	
+}
+
+#endif
